26a93899403657e7c75034ae41484877b7f84e62
chamer79 <chamer79@gmail.com>
2024-09-30 12:57:54 -0400

    server.js boilerplate. tested connection to MongoDB

---
+++ server.js
@@ -0,0 +1,55 @@
+// Imports
+const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const methodOverride = require("method-override")
+const morgan = require("morgan")
+
+// Import Controllers
+
+
+
+// App + Configurations
+dotenv.config()
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+// Middleware
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"))
+app.use(morgan("dev"))
+
+
+// DB Connection
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB: ${mongoose.connection.name}`)
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log(err)
+})
+
+
+// Whisky Routes
+
+
+
+
+
+
+
+// Review Routes
+
+
+
+
+
+// Server Handler
+app.listen(PORT, () => {
+    console.log(`Listening to PORT: ${PORT}`)
+})
