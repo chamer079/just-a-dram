490f67850a2032b0f54419e2ed0eecb4b972b62a
chamer79 <chamer79@gmail.com>
2024-09-30 13:21:34 -0400

    added psuedocode to whiskies.js

---
+++ controllers/whiskies.js
@@ -0,0 +1,34 @@
+// Import
+const Whisky = require("../models/Whisky")
+
+
+// Whisky Routes
+    // Whisky Index - GET -> "/whiskies"
+
+
+
+    // New Whisky - GET -> "/whiskies/new"
+
+
+
+    // Show Whisky - GET -> "/whiskies/:id"
+
+
+
+    // Post Whisky - POST -> "/whikies"
+
+
+
+    // Delete Whisky - DELETE -> "/whiskies/:id"
+
+
+
+    // Edit Whisky - GET -> "/whiskies/:id/edit"
+
+
+
+    // Update Whisky - PUT -> "/whiskies/:id"
+
+
+
+// Exports
