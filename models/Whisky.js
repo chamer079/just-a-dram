cd8e2eae56cc25ae238e6fa62cadb3f978131d3d
chamer79 <chamer79@gmail.com>
2024-09-30 13:14:18 -0400

    added whiskySchema & reviewSchema

---
+++ models/Whisky.js
@@ -0,0 +1,27 @@
+// Imports
+const mongoose = require("mongoose")
+
+// Schema
+const reviewSchema = new mongoose.Schema({
+    review: { type: String, required: true },
+    user: { type: String, required: true },
+})
+
+const whiskySchema = new mongoose.Schema({
+    whiakyImg: { type: String, requierd: true },
+    name: { type: String, required: true },
+    distillery: { type: String, required: true },
+    typeOfWhisky: { type: String, required: true },
+    age: { type: String, required: true },
+    proof: { type: Number, required: true },
+    abv: { type: String, required: true },
+    hue: { type: String, required: true },
+    flavor: { type: String, required: true },
+    reviews: [ reviewSchema ],
+})
+
+// Model
+const Whisky = mongoose.model("Whisky", whiskySchema)
+
+// Export
+module.exports = Whisky
